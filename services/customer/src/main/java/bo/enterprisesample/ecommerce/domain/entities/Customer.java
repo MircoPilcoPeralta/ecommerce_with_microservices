59d9bfb58f30e03274a1de035c822c9d744fdce0
Mirco874 <mircocuof@gmail.com>
2024-07-14 18:43:06 -0400

    Add entities

---
+++ services/customer/src/main/java/bo/enterprisesample/ecommerce/domain/entities/Customer.java
@@ -0,0 +1,25 @@
+package bo.enterprisesample.ecommerce.domain.entities;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Document
+@Builder
+public class Customer {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Address address;
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName;
+    }
+}
