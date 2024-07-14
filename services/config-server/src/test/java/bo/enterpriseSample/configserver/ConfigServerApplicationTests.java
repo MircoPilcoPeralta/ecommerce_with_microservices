227c361ecb1ad5b8e8791c39763b54d8aadea372
Mirco874 <mircocuof@gmail.com>
2024-07-14 13:42:40 -0400

    Add config-server service

---
+++ services/config-server/src/test/java/bo/enterpriseSample/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.enterpriseSample.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
